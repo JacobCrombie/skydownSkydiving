c8b5c4b9dda6df2ee5b69ba02cfee1604de61c72
Darbyblinko <jacobcrombie@live.com>
2021-07-27 18:27:32 -0600

    added links to navbar, changed to a component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="nav-bar">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <router-link class="navbar-brand" to="/">skydown</router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/tandem">Tandem</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/experienced">Experienced</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about">About</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'nav-bar',
+  data(){
+    return {}
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
